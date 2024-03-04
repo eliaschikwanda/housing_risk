877ecefa1a441f2a8018863059ec47ad91c2140e
Elias Chikwanda <chikwandaelias@gmail.com>
2024-03-04 14:11:20 -0800

    Project starter files

---
+++ housing_risk/housing_risk.py
@@ -0,0 +1,16 @@
+"""Welcome to Reflex!."""
+
+from housing_risk import styles
+
+# Import all the pages.
+from housing_risk.pages import *
+
+import reflex as rx
+
+
+class State(rx.State):
+    """Define empty state to allow access to rx.State.router."""
+
+
+# Create the app.
+app = rx.App(style=styles.base_style)
---
+++ rxconfig.py
@@ -0,0 +1,5 @@
+import reflex as rx
+
+config = rx.Config(
+    app_name="housing_risk",
+)
